974070bc9ee5fdbafd2e7018bcce6ea5f8cc27f0
ronaldscruz <ronald.scruz16@gmail.com>
2024-06-08 07:36:50 -0300

    chore: new favicon attempt

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,6 +36,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body className={montserrat.className}>{children}</body>
     </html>
   );
