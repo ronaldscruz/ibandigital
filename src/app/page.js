d2de8037eaee396ce51275c5719d315ec959a87b
ronaldscruz <ronald.scruz16@gmail.com>
2024-06-08 06:27:23 -0300

    chore: changed logo file to a better resolution

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Image className="pointer-events-none" src={'/iban-logo-full.png'} alt="Logo Iban Digital" width={380} height={200} />
+      <Image className="pointer-events-none" src={'/logo-full.png'} alt="Logo Iban Digital" width={380} height={200} />
       <h1 className="text-lg font-medium">Em breve...</h1>
     </main>
   );
