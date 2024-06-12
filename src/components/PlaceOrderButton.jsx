4a4d3633111ebb15fb783145e2a923eaa9b9b57c
ronaldscruz <ronald.scruz16@gmail.com>
2024-06-12 09:49:56 -0300

    chore: add aria-label to order button

--- src/components/PlaceOrderButton.jsx
+++ src/components/PlaceOrderButton.jsx
@@ -4,7 +4,8 @@ export default function PlaceOrderButton({ className = "" }) {
       href="https://wa.me/+5511965270711"
       target="_blank"
       rel="noreferrer"
-      className="cursor-pointer "
+      className="cursor-pointer"
+      aria-label="Solicite um orçamento"
     >
       <button
         className={`bg-blue-600 h-12 px-6 border-radius text-white font-medium rounded-full hover:bg-blue-700 ease-in-out duration-200 ${className}`}
