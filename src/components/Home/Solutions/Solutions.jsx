21bef46757997f55b9c695ceb2b713a54b549258
ronaldscruz <ronald.scruz16@gmail.com>
2024-06-24 00:10:36 -0300

    feat: fix menu redirects; change home banner text

--- src/components/Home/Solutions/Solutions.jsx
+++ src/components/Home/Solutions/Solutions.jsx
@@ -75,7 +75,10 @@ export default function Solutions() {
 
   return (
     <section className="flex w-full w-full flex justify-center items-center py-20">
-      <div className="w-content flex flex-col items-center relative md:px-0 px-8">
+      <div
+        id="solutions"
+        className="w-content flex flex-col items-center relative md:px-0 px-8"
+      >
         <h1 className="mb-2 font-medium md:text-5xl text-4xl text-center text-transparent py-2 bg-clip-text bg-gradient-to-r from-blue-500 to-indigo-600 leading-normal">
           Soluções que fazem a diferença
         </h1>
