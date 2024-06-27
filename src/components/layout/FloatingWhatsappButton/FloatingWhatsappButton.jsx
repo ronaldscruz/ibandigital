b91a8773a534cc5c1291e74942b6969743f51c88
ronaldscruz <ronald.scruz16@gmail.com>
2024-06-26 22:59:58 -0300

    chore: whatsapp new tab

--- src/components/layout/FloatingWhatsappButton/FloatingWhatsappButton.jsx
+++ src/components/layout/FloatingWhatsappButton/FloatingWhatsappButton.jsx
@@ -9,7 +9,11 @@ export default function FloatingWhatsappButton() {
       id="floating-whatsapp-button"
       className="fixed right-[26px] bottom-[26px] w-[64px] h-[64px] 2xl:w-[84px] 2xl:h-[84px] bg-green-500 rounded-full z-30"
     >
-      <a href="https://wa.me/551151960569?text=Ol%C3%A1%21%20Venho%20atrav%C3%A9s%20do%20site%20da%20Iban%20Digital%20e%20gostaria%20de%20receber%20atendimento%20especializado.">
+      <a
+        href="https://wa.me/551151960569?text=Ol%C3%A1%21%20Venho%20atrav%C3%A9s%20do%20site%20da%20Iban%20Digital%20e%20gostaria%20de%20receber%20atendimento%20especializado."
+        target="_blank"
+        rel="noreferrer"
+      >
         <button className="flex items-center justify-center">
           <Image
             src={WhatsappSvg}
